9ff1afd7d60af90db15939dc8943c55b4766d70e
Adrienne Yao <adrienne.yao@icloud.com>
2021-01-10 03:13:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,4 +10,7 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function displayMessage(msg) {
+  document.getElementById("greeting").innerText = msg;
+}
 /* Write your implementation of displayMessage() */
